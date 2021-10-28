fa562592198173aa580d0e4b99634be9e46e95e3
Rachel <rachelchen26@marlborough.org>
2021-10-28 13:20:38 -0700

    hi

--- My Failed Game Try 2/Game.java
+++ My Failed Game Try 2/Game.java
@@ -4,11 +4,30 @@ public class Game extends PApplet{
     size(1400, 800);
   }
   public void setup(){
-    
+    p = new Player(this);
   }
   public void draw(){
     background(0);
     p.display();
+  }
+  public void keyPressed(){
+    if(key == 'w'){
+      p.setVY(-5);
+    }else if(key == 'a'){
+      p.setVX(-5);
+    }else if(key == 's'){
+      p.setVY(5);
+    }else if(key == 'd'){
+      p.setVX(5);
+    }
+  }
+  public void keyReleased(){
+    if(key == 'w' || key == 's'){
+      p.setVY(0);
+    }
+    if(key == 'a' || key == 'd'){
+      p.setVX(0);
+    }
   }
   public static void main(String[] args){
     PApplet.main("Game");
--- My Failed Game Try 2/Player.java
+++ My Failed Game Try 2/Player.java
@@ -2,17 +2,31 @@ import processing.core.*;
 public class Player{
   public Player(PApplet p){
     this.p = p;
-    this. c = p.color(255, 0, 107);
+    this.c = p.color(255, 0, 107);
     this.s = 30;
+    this.x = 60;
+    this.y = p.height - 60;
+    this.vx = 0;
+    this.vy = 0;
   }
   public void display(){
     p.fill(c);
-    p.rect(30, 30, s, s);
+    p.rect(x, y, s, s);
+    x += vx;
+    y += vy;
   }
-  public void keyPressed(){
+  public void setVX(int n){
+    vx = n;
+  }
+  public void setVY(int n){
+    vy = n;
+  }
 
-  }
   private PApplet p;
   private int c;
   private float s;
+  private float x;
+  private float y;
+  private int vx;
+  private int vy;
 }
